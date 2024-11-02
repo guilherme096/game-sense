cc699ea35c6af1fde5a866cd8560eedb3eff2289
ruimachado23 <rui.faria.machado@gmail.com>
2024-11-02 17:22:19 +0000

    feat(player_service): player service controller and enpoints

--- src/player_service/src/main/java/ies/gamesense/player_service/PlayerController.java
+++ src/player_service/src/main/java/ies/gamesense/player_service/PlayerController.java
@@ -9,5 +9,20 @@ public class PlayerController {
 
     @Autowired
     private PlayerService playerService;
-    
+
+    @GetMapping("/players")
+    public Iterable<Player> getPlayers() {
+        return playerService.getPlayers();
+    }
+
+    @GetMapping("/{id}")
+    public Player getPlayer(@PathVariable Long id) {
+        return playerService.getPlayer(id);
+    }
+
+    // GET /api/v1/player/search?name=John&club=Barcelona
+    @GetMapping("/search")
+    public Iterable<Player> searchPlayers(@RequestParam(required = false) String name, @RequestParam(required = false) String club) {
+        return playerService.searchPlayers(name, club);
+    }
 }
