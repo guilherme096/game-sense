0b8070221b3e08d668efd87d0ce26c50cdd58bef
guilherme096 <guilherme.rosa60@gmail.com>
2024-11-04 18:26:26 +0000

    doc(swagger): add label to each endpoint

--- src/live_game_service/src/main/java/ies/gamesense/live_game_service/controllers/LiveController.java
+++ src/live_game_service/src/main/java/ies/gamesense/live_game_service/controllers/LiveController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ies.gamesense.live_game_service.entities.GameStatistics;
 import ies.gamesense.live_game_service.entities.Live;
 import ies.gamesense.live_game_service.services.LiveService;
+import io.swagger.v3.oas.annotations.Operation;
 
 /**
  * LiveController
@@ -26,6 +27,7 @@ public class LiveController {
     @Autowired
     private LiveService liveService;
 
+    @Operation(summary = "Get live game by id")
     @GetMapping("/{id}")
     public ResponseEntity<Live> getLiveGame(@PathVariable("id") long id) {
         Live game = liveService.getLiveById(id);
@@ -33,6 +35,7 @@ public class LiveController {
         return new ResponseEntity<>(game, gameExists ? HttpStatus.OK : HttpStatus.NOT_FOUND);
     }
 
+    @Operation(summary = "Get game statistics by game id")
     @GetMapping("/{id}/statistics")
     public ResponseEntity<GameStatistics> getGameStatistics(@PathVariable("id") long id) {
         GameStatistics gameStatistics = liveService.getGameStatistics(id);
@@ -40,6 +43,7 @@ public class LiveController {
         return new ResponseEntity<>(gameStatistics, gameStatisticsExists ? HttpStatus.OK : HttpStatus.NOT_FOUND);
     }
 
+    @Operation(summary = "Get new events for live game")
     @GetMapping("/{id}/ping")
     public ResponseEntity<List<Map<String, String>>> ping(@PathVariable("id") long id,
             @RequestParam("lastEventId") long lastEventId) {
