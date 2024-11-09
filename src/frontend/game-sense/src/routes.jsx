87510277f489fe43400f69dec7a316aa792581d6
ruimachado23 <rui.faria.machado@gmail.com>
2024-11-09 12:49:53 +0000

    feat(navbar-header): navbar and header done

--- src/frontend/game-sense/src/routes.jsx
+++ src/frontend/game-sense/src/routes.jsx
@@ -1,7 +1,12 @@
 import Home from "./routes/Home";
+import Template from "./routes/PageTemplate";
+
 export const routesList = [
   {
     path: "/",
-    element: <Home />,
+    element: <Home />, 
+
+    path: "/template",
+    element: <Template />,
   },
-];
+];  
---
+++ src/frontend/game-sense/src/routes/PageTemplate.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar/Navbar";
+import Header from "../components/Header";
+
+function PageTemplate() {
+    return (
+      <>
+        <Header></Header>
+        <Navbar></Navbar>
+      </>
+    );
+  }
+  
+  export default PageTemplate;
+  
