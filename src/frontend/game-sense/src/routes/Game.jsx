6c731151575e27eafcf90a5c0d9fcc3592a9db65
guilherme096 <guilherme.rosa60@gmail.com>
2024-11-10 23:21:01 +0000

    ref(overivew): seperate overview tab from game page

--- src/frontend/game-sense/src/routes/Game.jsx
+++ src/frontend/game-sense/src/routes/Game.jsx
@@ -1,39 +1,12 @@
 import Navbar from "../components/Navbar/Navbar";
 import Header from "../components/Header";
-import GameTimeline from "../components/GamePage/GameTimeLine.jsx";
-import MatchInformationCard from "../components/GamePage/MatchInformationCard.jsx";
-import MvpCard from "../components/GamePage/MvpCard.jsx";
-import MostSomethingCard from "../components/GamePage/MostSomethingCard.jsx";
-
-import { matchInformation, mvp, mostSomethingCategories, gameEvents } from "../static/game.js";
+import Overview from "./Overview.jsx";
 
 function Game() {
   return (
     <>
       <Header />
-      <div className="p-4">
-        <GameTimeline events={gameEvents} />
-      </div>
-      <div className="row flex space-x-3">
-        {/* Left Side - Match Information */}
-        <div className="col w-1/2 pl-4">
-          <MatchInformationCard
-            kickoff={matchInformation.kickoff}
-            referee={matchInformation.referee}
-            stadium={matchInformation.stadium}
-          />
-        </div>
-
-        {/* Right Side - MVP and Carousel */}
-        <div className="col flex-col w-1/2 space-y-3 pr-4">
-          <div className="row">
-            <MvpCard name={mvp.name} score={mvp.score} />
-          </div>
-          <div className="row pb-3">
-            <MostSomethingCard categories={mostSomethingCategories} />
-          </div>
-        </div>
-      </div>
+      <Overview />
       <Navbar />
     </>
   );
