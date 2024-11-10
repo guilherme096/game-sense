be9985a92fa50b4890fb277fbe3df9303fe98d60
ruimachado23 <rui.faria.machado@gmail.com>
2024-11-10 14:57:47 +0000

    feat(game-overview): information tabs

---
+++ src/frontend/game-sense/src/static/game.js
@@ -0,0 +1,24 @@
+export const gameEvents = [
+    { minute: 8, description: "Rui (Roger)", type: "substitution", team: "right" },
+    { minute: 15, description: "Samu", type: "goal", team: "left" },
+    { minute: 23, description: "Bruma", type: "red-card", team: "right" },
+    { minute: 29, description: "Esgaio (AG)", type: "auto-goal", team: "left" },
+    { minute: 43, description: "Moutinho", type: "yellow-card", team: "right" },
+];
+
+export const matchInformation = {
+    kickoff: "Today at 22h30",
+    referee: "Martim dos Santos",
+    stadium: "Estádio do Dragão",
+};
+
+export const mvp = {
+    name: "S. Omorodion",
+    score: 9.3,
+};
+
+export const mostSomethingCategories = [
+    { category: "Passes", name: "Alan Varela", value: 23 },
+    { category: "Shots", name: "John Doe", value: 7 },
+    { category: "Tackles", name: "Jane Smith", value: 5 },
+];
