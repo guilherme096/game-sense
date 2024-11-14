349d32c7d97581554b5edc45a097b0a19cba63aa
Ric <henriqueft04@ua.pt>
2024-11-14 16:07:49 +0000

    feature: frontend-> turned the score board into a dynamic component

---
+++ src/frontend/game-sense/src/components/GamePage/ScoreBoard.jsx
@@ -0,0 +1,78 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import PropTypes from 'prop-types';
+import {faShareNodes} from "@fortawesome/free-solid-svg-icons/faShareNodes";
+
+
+export default function ScoreBoard({ team1, team2, score1, score2, minute }) {
+    return(
+        <>
+            <div className="w-full text-center font-semibold">{minute <= 45 ? '1st Half' : (minute >= 46 && minute < 90 ? '2nd Half' : 'Full Time')}</div>
+            <div className="w-full h-fit flex flex-row justify-between align-middle items-center">
+                <div>
+                    <div className="flex flex-row items-center">
+                        <button>
+                            <FontAwesomeIcon
+                                icon={faStar}
+                                style={{color: team1.stared ? "#FFD43B" : "#D1D5DB"}}
+                                className="w-6 h-6 mr-2"
+                            />
+                        </button>
+                        <div className="w-24 h-24 bg-base-200 rounded-lg flex items-center justify-center overflow-hidden">
+                            {team1.image ? (
+                                <img
+                                    className="w-full h-full object-contain"
+                                    src={team1.image}
+                                    alt={team1.name}
+                                />
+                            ) : (
+                                <span className="text-sm text-gray-500">{team1.name}</span>
+                            )}
+                        </div>
+                    </div>
+                    <div className="text-center my-2 ml-8">{team1.name}</div>
+                </div>
+                <div className="h-fit flex flex-col align-middle items-center">
+                    <div className="h-24 flex-col items-center align-middle flex justify-center">
+                        <div className="w-fit h-fit text-4xl">{score1} - {score2}</div>
+                        <div className="w-fit h-fit text-sm text-center">{minute}'</div>
+                    </div>
+                    <button className="text-center text-sm flex justify-center font-extralight items-center">
+                        <img src='public/icons8-share-256 1.png' alt="Share Icon"/>
+                    </button>
+                </div>
+                <div>
+                <div className="flex flex-row items-center">
+                        <div className="w-24 h-24 bg-base-200 rounded-lg flex items-center justify-center overflow-hidden">
+                        {team2.image ? (
+                                <img
+                                    className="w-full h-full object-contain"
+                                    src={team2.image}
+                                    alt={team2.name}
+                                />
+                            ) : (
+                                <span className="text-sm text-gray-500">{team2.name}</span>
+                            )}
+                        </div>
+                        <button>
+                            <FontAwesomeIcon
+                                icon={faStar}
+                                style={{color: team2.stared ? "#FFD43B" : "#D1D5DB"}}
+                                className="w-6 h-6 ml-2"
+                            />
+                        </button>
+                    </div>
+                    <div className="text-center my-2 mr-8">{team2.name}</div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+ScoreBoard.propTypes = {
+    team1: PropTypes.object,
+    team2: PropTypes.object,
+    score1: PropTypes.number,
+    score2: PropTypes.number,
+    minute: PropTypes.number
+}
--- src/frontend/game-sense/src/static/game.js
+++ src/frontend/game-sense/src/static/game.js
@@ -22,3 +22,19 @@ export const mostSomethingCategories = [
     { category: "Shots", name: "John Doe", value: 7 },
     { category: "Tackles", name: "Jane Smith", value: 5 },
 ];
+
+export const scoreBoardInfo = {
+    team1: {
+        name: "FC Porto",
+        image: "https://upload.wikimedia.org/wikipedia/pt/c/c5/F.C._Porto_logo.png",
+        stared: true
+    },
+    team2: {
+        name: "Sporting CP",
+        stared: false
+    },
+    score1: 1,
+    score2: 1,
+    half: 1,
+    minute: 35,
+};
