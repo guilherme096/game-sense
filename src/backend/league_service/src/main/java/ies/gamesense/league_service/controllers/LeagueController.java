05c27915b6e6406e4e20217893070ad746be720f
Roldão <jroldao04@ua.pt>
2024-11-16 15:51:11 +0000

    feature(league-page): connect to API

--- src/backend/league_service/src/main/java/ies/gamesense/league_service/controllers/LeagueController.java
+++ src/backend/league_service/src/main/java/ies/gamesense/league_service/controllers/LeagueController.java
@@ -7,12 +7,22 @@ import ies.gamesense.league_service.services.LeagueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/league")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173"})
 public class LeagueController {
 
     @Autowired
--- src/frontend/game-sense/src/routes/LeaguePageBase.jsx
+++ src/frontend/game-sense/src/routes/LeaguePageBase.jsx
@@ -2,13 +2,51 @@ import EntityCard from "../components/cards/EntityCard.jsx"
 import PageTemplate from "./PageTemplate.jsx";
 import Standings from "../components/standings/Standings.jsx"
 import HorizontalTab from "../components/HorizontalTab.jsx";
+import {useQuery, useQueryClient} from 'react-query';
+import axios from 'axios';
+
+
+const fetchGame = async () => {
+    const response = await axios.get("http://localhost:8084/api/v1/league/1", {
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            'Accept': 'application/json'
+        }
+    });
+    return response.data;
+}
+
 
 function LeaguePageBase({ children }) {
+    const {data: league, isLoading, error} = useQuery('league', fetchGame);
+    console.log(league);
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    if (error) {
+        return <div>An error has occurred: {error.message}</div>
+    }
+
+    const name = league.name;
+    const image = league.logo;
+
+    const sortedStandings = league.standings
+    .map((team) => ({
+        ...team,
+        points: team.won * 3 + team.drawn,
+    }))
+    .sort((a, b) => {
+        if (b.points !== a.points) return b.points - a.points;
+        if (b.gs - b.gc !== a.gs - a.gc) return b.gs - b.gc - (a.gs - a.gc);
+    });
+
+
     return (
         <>
             <PageTemplate>
-                <EntityCard image='/pl.png' name='Premier'/>
-                <HorizontalTab color_back="bg-gray-700" categories={[ { name: 'Overview' , content: <Standings /> }, {name:'Next Games'}, {name:'Results'}, {name:'Statistics'} ]} />
+                <EntityCard image={image} name={name} />
+                <HorizontalTab color_back="bg-gray-700" categories={[ { name: 'Overview' , content: <Standings sortedStandings={sortedStandings}></Standings> }, {name:'Next Games'}, {name:'Results'}, {name:'Statistics'} ]} />
             </PageTemplate>
         </>
     );
