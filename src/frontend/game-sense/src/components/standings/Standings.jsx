106407de5c8cacc71ac1e80235e33e4589ba4a50
Ric <henriqueft04@ua.pt>
2024-11-17 18:50:30 +0000

    feature: -> frontend: inserted standings to the game horizontal tab + refactored some components

--- src/frontend/game-sense/src/components/standings/Standings.jsx
+++ src/frontend/game-sense/src/components/standings/Standings.jsx
@@ -1,7 +1,58 @@
 import TeamTable from './TeamTable';
 import GeneralCard from '../cards/GeneralCard';
+import {useQuery, useQueryClient} from 'react-query';
+import axios from 'axios';
 
-export default function Standings({sortedStandings}) {
+export default function Standings() {
+    
+    const fetchGame = async () => {
+        const response = await axios.get("http://localhost:8084/api/v1/league/1", {
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                'Accept': 'application/json'
+            }
+        });
+        return response.data;
+    }
+
+    const {data: league, isLoading, error} = useQuery('league', fetchGame);
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    if (error) {
+        return <div>An error has occurred: {error.message}</div>
+    }
+
+    console.log(league);
+
+    if (!Array.isArray(league.standings)) {
+        console.error("Standings is not an array:", league.standings);
+        return <div>Error: Invalid standings data</div>;
+    }
+
+    console.log(league.standings);
+
+    const name = league.name;
+    const image = league.logo;
+
+    const sortedStandings = league.standings
+        .map((team, index) => ({
+            ...team,
+            points: team.wins * 3 + team.draws,
+        }))
+        .sort((a, b) => {
+            if (b.points !== a.points) return b.points - a.points;
+            if (b.goalsScored - b.goalsConceded !== a.goalsScored - a.goalsConceded)
+                return (b.goalsScored - b.goalsConceded) - (a.goalsScored - a.goalsConceded);
+        })
+        .map((team, index) => ({
+            ...team,
+            position: index + 1,
+        }));    
+
+        console.log(sortedStandings);
+
 
     // Define the button as JSX
     const changeTeam = (
--- src/frontend/game-sense/src/routes/LeaguePageBase.jsx
+++ src/frontend/game-sense/src/routes/LeaguePageBase.jsx
@@ -5,18 +5,19 @@ import HorizontalTab from "../components/HorizontalTab.jsx";
 import {useQuery, useQueryClient} from 'react-query';
 import axios from 'axios';
 
-const fetchGame = async () => {
-    const response = await axios.get("http://localhost:8084/api/v1/league/1", {
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            'Accept': 'application/json'
-        }
-    });
-    return response.data;
-}
-
 function LeaguePageBase({ children }) {
+
+    const fetchGame = async () => {
+        const response = await axios.get("http://localhost:8084/api/v1/league/1", {
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                'Accept': 'application/json'
+            }
+        });
+        return response.data;
+    }
+
     const {data: league, isLoading, error} = useQuery('league', fetchGame);
     if (isLoading) {
         return <div>Loading...</div>
@@ -25,40 +26,16 @@ function LeaguePageBase({ children }) {
         return <div>An error has occurred: {error.message}</div>
     }
 
-    console.log(league);
-
-    if (!Array.isArray(league.standings)) {
-        console.error("Standings is not an array:", league.standings);
-        return <div>Error: Invalid standings data</div>;
-    }
-
-    console.log(league.standings);
-
     const name = league.name;
+    const name1 = name.split(' ')[0];
+    const name2 = name.split(' ')[1];
     const image = league.logo;
-
-    const sortedStandings = league.standings
-        .map((team, index) => ({
-            ...team,
-            points: team.wins * 3 + team.draws,
-        }))
-        .sort((a, b) => {
-            if (b.points !== a.points) return b.points - a.points;
-            if (b.goalsScored - b.goalsConceded !== a.goalsScored - a.goalsConceded)
-                return (b.goalsScored - b.goalsConceded) - (a.goalsScored - a.goalsConceded);
-        })
-        .map((team, index) => ({
-            ...team,
-            position: index + 1,
-        }));    
-
-        console.log(sortedStandings);
 
     return (
         <>
             <PageTemplate>
-                <EntityCard image={image} name={name} />
-                <HorizontalTab color_back="bg-gray-700" categories={[ { name: 'Overview' , content: <Standings sortedStandings={sortedStandings}></Standings> }, {name:'Next Games'}, {name:'Results'}, {name:'Statistics'} ]} />
+                <EntityCard image={image} name1={name1} name2={name2} />
+                <HorizontalTab  categories={[ { name: 'Overview' , content: <Standings /> }, {name:'Next Games'}, {name:'Results'}, {name:'Statistics'} ]} />
             </PageTemplate>
         </>
     );
