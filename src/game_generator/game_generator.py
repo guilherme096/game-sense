4c38ce6125eb783f820e377f8387d054557ea0e2
guilherme096 <guilherme.rosa60@gmail.com>
2024-11-28 12:42:53 +0000

    feat(game_generator): complete game_generator initial features

--- src/game_generator/game_generator.py
+++ src/game_generator/game_generator.py
@@ -1,5 +1,5 @@
 import random
-from models import *
+from models import Game, Stats, Team, Winner
 import logging
 from math import log
 
@@ -58,7 +58,7 @@ def generate_goals(home_team: Team, away_team: Team, seed: int, winner: Winner):
     goals = [0, 0]
 
     # generate activations
-    logging.debug(f"Activations:")
+    logging.debug("Activations:")
     for i, team in enumerate(teams):
         attack_strength = (team.attack_strength / 10) * ((team.form / 5) * 0.5)
 
@@ -66,14 +66,14 @@ def generate_goals(home_team: Team, away_team: Team, seed: int, winner: Winner):
         activations[i] = (attack_strength * activations[i]) / winner_modifier
         logging.debug(f"    Team: {team.id}")
         logging.debug(f"    Activation: {activations[i]}")
-        logging.debug(f"----------------")
+        logging.debug("----------------")
 
     # generate winner goals
 
     delta = 1
     multiplier = 0.3
     index = 1
-    logging.debug(f"Winner goals:")
+    logging.debug("Winner goals:")
     while True:
         rnd = random.randrange(0, 8) * 0.1
         logging.debug(f"    Random: {rnd}")
@@ -84,7 +84,7 @@ def generate_goals(home_team: Team, away_team: Team, seed: int, winner: Winner):
             logging.debug(f"    Goal: {goals[winner_idx]}")
             logging.debug(f"    Delta: {delta}")
             logging.debug(f"    Modifier: {multiplier}")
-            logging.debug(f"----------------")
+            logging.debug("----------------")
         else:
             break
 
@@ -122,18 +122,15 @@ def generate_stats(winner: Team, loser: Team, winner_score: int, loser_score: in
     random.seed(seed)
     activations = [random.randrange(5, 10) * 0.1, random.randrange(3, 8) * 0.1]
 
-    w_attack_strength = (winner.attack_strength / 10) * \
-        ((winner.form / 5) * 0.5)
+    w_attack_strength = (winner.attack_strength / 10) * ((winner.form / 5) * 0.5)
 
     l_attack_strength = (loser.attack_strength / 10) * ((loser.form / 5) * 0.5)
 
     activations[0] = (w_attack_strength * activations[0]) / 0.4
     activations[1] = (l_attack_strength * activations[1]) / 0.8
 
-    w_activations = [max(random.randrange(4, 10) * activations[0], 1)
-                     for _ in range(8)]
-    l_activations = [max(random.randrange(4, 10) * activations[1], 1)
-                     for _ in range(8)]
+    w_activations = [max(random.randrange(4, 10) * activations[0], 1) for _ in range(8)]
+    l_activations = [max(random.randrange(4, 10) * activations[1], 1) for _ in range(8)]
 
     w_possession = max(round(w_activations[0] * 10), 30)
     w_stats = Stats(
@@ -172,8 +169,7 @@ def generate_game(home_team: Team, away_team: Team, seed: int):
     game.home_score = home_goals
     game.away_score = away_goals
 
-    w_stats, l_stats = generate_stats(
-        home_team, away_team, home_goals, away_goals)
+    w_stats, l_stats = generate_stats(home_team, away_team, home_goals, away_goals)
 
     home_team.set_stats(w_stats)
     away_team.set_stats(l_stats)
