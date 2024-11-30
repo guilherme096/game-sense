2e6b56717e728c97195b37e375536ed5345f518c
Roldão <jroldao04@ua.pt>
2024-11-30 13:31:05 +0000

    Fix REST convetion for club service

--- src/backend/club_service/src/main/java/ies/gamsense/club_service/controller/ClubController.java
+++ src/backend/club_service/src/main/java/ies/gamsense/club_service/controller/ClubController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -27,7 +27,7 @@ public class ClubController {
     private ClubService clubService;
 
     @Operation(summary = "Get all Clubs")
-    @GetMapping("/clubs")
+    @GetMapping("/")
     public List<Club> getAllClubs() {
         return clubService.getAllClubs();
     }
@@ -39,13 +39,13 @@ public class ClubController {
     }
 
     @Operation(summary = "Search Clubs by Name")
-    @GetMapping("/search")
+    @GetMapping(params = "name")
     public List<Club> getClubsByName(@Parameter(description = "Name of the club") @RequestParam String name) {
         return clubService.getClubsByName(name);
     }
 
     @Operation(summary = "Star a Club")
-    @PostMapping("/star")
+    @PutMapping("/{id}/star")
     public Club starClub(@Parameter(description = "ID of the club you want to star") Long clubId) {
         return clubService.starClub(clubId);
     }
