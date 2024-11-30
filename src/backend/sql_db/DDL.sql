fdb7ecfb4cd4d9bac5deb2f0bdb29ceb58916d01
ruimachado23 <rui.faria.machado@gmail.com>
2024-11-30 22:58:39 +0000

    feature(db): create schema in docker

--- src/backend/sql_db/DDL.sql
+++ src/backend/sql_db/DDL.sql
@@ -1,20 +1,18 @@
 CREATE TABLE IF NOT EXISTS league(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(254) NOT NULL,
+    name VARCHAR(50) NOT NULL,
     logo VARCHAR(254) NOT NULL
-)
-GO
+);
 
 CREATE TABLE IF NOT EXISTS club(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(254) NOT NULL,
-    country VARCHAR(254) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    country VARCHAR(50) NOT NULL,
     starred BOOLEAN NOT NULL,
     image VARCHAR(254) NOT NULL
-)
-GO
+);
 
-CREATE TABLE IF NOT EXISTS league_club(
+CREATE TABLE IF NOT EXISTS league_club (
     league_id INT NOT NULL,
     club_id INT NOT NULL,
     matches_played INT NOT NULL,
@@ -29,19 +27,18 @@ CREATE TABLE IF NOT EXISTS league_club(
     PRIMARY KEY (league_id, club_id),
     FOREIGN KEY (league_id) REFERENCES league(id),
     FOREIGN KEY (club_id) REFERENCES club(id)
-)
-GO
+);
 
-CREATE TABLE IF NOT EXISTS player(
+CREATE TABLE IF NOT EXISTS player (
     id INT AUTO_INCREMENT PRIMARY KEY,
     club_id INT NOT NULL,
-    name VARCHAR(254) NOT NULL,
-    surname VARCHAR(254) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
     birth_date DATE NOT NULL,
     height FLOAT NOT NULL,
     weight FLOAT NOT NULL,
-    position VARCHAR(254) NOT NULL,
-    country VARCHAR(254) NOT NULL,
+    position VARCHAR(50) NOT NULL,
+    country VARCHAR(50) NOT NULL,
     goals INT NOT NULL,
     assists INT NOT NULL,
     minutes INT NOT NULL,
@@ -51,64 +48,72 @@ CREATE TABLE IF NOT EXISTS player(
     saves INT NOT NULL,
     penalties INT NOT NULL,
     FOREIGN KEY (club_id) REFERENCES club(id)
-)
-GO
+);
 
-CREATE TABLE IF NOT EXISTS game(
+CREATE TABLE IF NOT EXISTS game (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    referee VARCHAR(254) NOT NULL,
+    referee VARCHAR(50) NOT NULL,
     kickoff_time TIMESTAMP NOT NULL,
-    stadium VARCHAR(254) NOT NULL,
+    stadium VARCHAR(50) NOT NULL,
     minute_played INT NOT NULL,
     home_team_id INT NOT NULL,
     away_team_id INT NOT NULL,
-    mvp VARCHAR(254) NOT NULL,
+    mvp VARCHAR(50) NOT NULL,
     FOREIGN KEY (away_team_id) REFERENCES club(id),
     FOREIGN KEY (home_team_id) REFERENCES club(id)
-)
-GO
+);
 
-CREATE TABLE IF NOT EXISTS top_stats(
-    game_id INT NOT NULL FOREIGN KEY REFERENCES game(id),
-    player_id INT NOT NULL FOREIGN KEY REFERENCES player(id),
-    stat VARCHAR(254) NOT NULL,
+
+CREATE TABLE IF NOT EXISTS top_stats (
+    game_id INT NOT NULL,
+    player_id INT NOT NULL,
+    stat VARCHAR(50) NOT NULL,
     value INT NOT NULL,
-    PRIMARY KEY (game_id, stat)
-)
-GO
+    PRIMARY KEY (game_id, stat),
+    FOREIGN KEY (game_id) REFERENCES game(id) ON DELETE CASCADE,
+    FOREIGN KEY (player_id) REFERENCES player(id) ON DELETE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS half(
-    game_id INT NOT NULL FOREIGN KEY REFERENCES game(id),
-    team_id INT NOT NULL FOREIGN KEY REFERENCES club(id),
+
+CREATE TABLE IF NOT EXISTS half (
+    id INT AUTO_INCREMENT PRIMARY KEY,  
+    game_id INT NOT NULL,
+    team_id INT NOT NULL,
     minute_played INT NOT NULL,
-    possession INT NOT NULL,
-    shots INT NOT NULL,
-    passes_acc INT NOT NULL,
-    tackles INT NOT NULL,
-    fouls INT NOT NULL,
-    corners INT NOT NULL
-)
-GO
-
-CREATE TABLE IF NOT EXISTS injury(
-    player_id INT NOT NULL FOREIGN KEY REFERENCES player(id),
-    date DATE NOT NULL,
-    name VARCHAR(254) NOT NULL,
-    duration INT NOT NULL,
-    PRIMARY KEY (player_id, date)
-)
-GO
-
-CREATE TABLE IF NOT EXISTS team_stats(
-    game_id INT NOT NULL FOREIGN KEY REFERENCES game(id),
-    half_id INT NOT NULL FOREIGN KEY REFERENCES half(id),
-    club_id INT NOT NULL FOREIGN KEY REFERENCES club(id),
     possession INT NOT NULL,
     shots INT NOT NULL,
     passes_acc INT NOT NULL,
     tackles INT NOT NULL,
     fouls INT NOT NULL,
     corners INT NOT NULL,
-    offsides INT NOT NULL
-)
-GO
+    FOREIGN KEY (game_id) REFERENCES game(id),
+    FOREIGN KEY (team_id) REFERENCES club(id)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS injury (
+    player_id INT NOT NULL,
+    date DATE NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    duration INT NOT NULL,
+    PRIMARY KEY (player_id, date),
+    FOREIGN KEY (player_id) REFERENCES player(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS team_stats (
+    game_id INT NOT NULL,
+    half_id INT NOT NULL,
+    club_id INT NOT NULL,
+    possession INT NOT NULL,
+    shots INT NOT NULL,
+    passes_acc INT NOT NULL,
+    tackles INT NOT NULL,
+    fouls INT NOT NULL,
+    corners INT NOT NULL,
+    offsides INT NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES game(id),
+    FOREIGN KEY (half_id) REFERENCES half(id),
+    FOREIGN KEY (club_id) REFERENCES club(id)
+);
