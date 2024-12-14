2b18952fa8db868f0416fbffbf2acd211adec661
Roldão <jroldao04@ua.pt>
2024-12-14 13:00:32 +0000

    fix league standings issue

--- src/frontend/game-sense/src/components/standings/TeamInfo.jsx
+++ src/frontend/game-sense/src/components/standings/TeamInfo.jsx
@@ -66,7 +66,7 @@ export default function TeamInfo({ team, index }) {
             height: 20,
           }}
         >
-          {team.position}
+          {team.place}
         </div>
       </td>
 
