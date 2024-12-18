db9c1fdc04c2c47981f725f71c9f6d360becafe2
guilherme096 <guilherme.rosa60@gmail.com>
2024-12-18 22:43:46 +0000

    fix game sorting events and home page crashing after leaving game

--- src/frontend/game-sense/src/components/GamePage/ScoreBoard.jsx
+++ src/frontend/game-sense/src/components/GamePage/ScoreBoard.jsx
@@ -31,7 +31,9 @@ export default function ScoreBoard({ id }) {
         error,
         isLoading,
     } = useQuery("game", () => fetchGame(id), {
-        refetchInterval: 10000,
+        refetchInterval: (data) => {
+            return data?.ended ? false : 10000;
+        },
     });
 
     if (isLoading) {
@@ -75,9 +77,14 @@ export default function ScoreBoard({ id }) {
                             />
                         </button>
                     </div>
-                    <div className="flex flex-col items-center cursor-pointer" onClick={() => navigate(`/club/${game.home_team_id}`)}>
-                        <div className="w-24 h-24 bg-base-200 rounded-lg flex items-center justify-center overflow-hidden cursor-pointer"
-                            onClick={() => navigate(`/club/${game.home_team_id}`)}>
+                    <div
+                        className="flex flex-col items-center cursor-pointer"
+                        onClick={() => navigate(`/club/${game.home_team_id}`)}
+                    >
+                        <div
+                            className="w-24 h-24 bg-base-200 rounded-lg flex items-center justify-center overflow-hidden cursor-pointer"
+                            onClick={() => navigate(`/club/${game.home_team_id}`)}
+                        >
                             {logo1 ? (
                                 <img
                                     className="w-full h-full object-contain p-2"
@@ -85,10 +92,12 @@ export default function ScoreBoard({ id }) {
                                     alt={`${name1}'s logo`}
                                 />
                             ) : (
-                                <span className="text-sm text-gray-500 flex items-center justify-center h-full">{name1}'s logo</span>
+                                <span className="text-sm text-gray-500 flex items-center justify-center h-full">
+                                    {name1}'s logo
+                                </span>
                             )}
                         </div>
-                        <div className="text-center mt-2" >{name1}</div>
+                        <div className="text-center mt-2">{name1}</div>
                     </div>
                 </div>
                 <div className="h-fit flex flex-col align-middle items-center">
@@ -106,7 +115,10 @@ export default function ScoreBoard({ id }) {
                 </div>
 
                 <div className="flex flex-row items-center">
-                    <div className="flex flex-col items-center cursor-pointer" onClick={() => navigate(`/club/${game.away_team_id}`)}>
+                    <div
+                        className="flex flex-col items-center cursor-pointer"
+                        onClick={() => navigate(`/club/${game.away_team_id}`)}
+                    >
                         <div className="w-24 h-24 bg-base-200 text-center rounded-lg flex items-center justify-center overflow-hidden">
                             {logo2 ? (
                                 <img
@@ -115,10 +127,12 @@ export default function ScoreBoard({ id }) {
                                     alt={`${name2}'s logo`}
                                 />
                             ) : (
-                                <span className="text-sm text-gray-500 flex items-center justify-center h-full w-full">{name2}'s logo</span>
+                                <span className="text-sm text-gray-500 flex items-center justify-center h-full w-full">
+                                    {name2}'s logo
+                                </span>
                             )}
                         </div>
-                        <div className="text-center mt-2" >{name2}</div>
+                        <div className="text-center mt-2">{name2}</div>
                     </div>
                     <div className="flex items-center ml-2 mb-6">
                         <button onClick={() => setTeam2Stared(!team2Stared)}>
@@ -139,5 +153,4 @@ export default function ScoreBoard({ id }) {
 
 ScoreBoard.propTypes = {
     id: PropTypes.string.isRequired,
-
 };
