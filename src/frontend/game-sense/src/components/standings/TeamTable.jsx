566f2f2c6e431ef80aadc2c7f65d74ac632bee6d
ruimachado23 <rui.faria.machado@gmail.com>
2024-12-18 19:11:57 +0000

    fix(navbar): overlap

--- src/frontend/game-sense/src/components/standings/TeamTable.jsx
+++ src/frontend/game-sense/src/components/standings/TeamTable.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 export default function TeamTable({ standings }) {
   console.log("team table component");
   return (
+    <div>
       <div className="overflow-x-auto rounded-xl">
         <table className="table-auto w-full text-sm text-left" style={{ tableLayout: 'fixed' }}>
           {/* Column Group to Define Widths */}
@@ -20,32 +21,35 @@ export default function TeamTable({ standings }) {
 
           {/* Table Header */}
           <thead className="bg-gray-100 border-b">
-          <tr>
-            <th className="px-2 py-2 text-center">#</th>
-            <th className="px-2 py-2">Club</th>
-            <th className="px-2 py-2 text-center">MP</th>
-            <th className="px-2 py-2 text-center">W</th>
-            <th className="px-2 py-2 text-center">D</th>
-            <th className="px-2 py-2 text-center">L</th>
-            <th className="px-2 py-2 text-center">GD</th>
-            <th className="px-2 py-2 text-center">P</th>
-          </tr>
+            <tr>
+              <th className="px-2 py-2 text-center">#</th>
+              <th className="px-2 py-2">Club</th>
+              <th className="px-2 py-2 text-center">MP</th>
+              <th className="px-2 py-2 text-center">W</th>
+              <th className="px-2 py-2 text-center">D</th>
+              <th className="px-2 py-2 text-center">L</th>
+              <th className="px-2 py-2 text-center">GD</th>
+              <th className="px-2 py-2 text-center">P</th>
+            </tr>
           </thead>
 
           {/* Table Body */}
           <tbody>
-          {standings.map((team, index) => (
+            {standings.map((team, index) => (
               <TeamInfo key={index} team={team} />
-          ))}
+            ))}
           </tbody>
         </table>
       </div>
+      <div className='pb-28'></div>
+    </div>
+
   );
 
 }
 
 TeamTable.propTypes = {
-    standings: PropTypes.array.isRequired,
-    showHeader: PropTypes.bool,
+  standings: PropTypes.array.isRequired,
+  showHeader: PropTypes.bool,
 
 };
